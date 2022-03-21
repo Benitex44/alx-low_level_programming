757e7e05b11fea5e9593dc09a8b3076473deab52
Benitex44 <benbenedict971@gmail.com>
2022-03-21 17:02:21 +0100

    my new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
 /*
  *main - check the code. Return: Always 0.
  */
-void reset_to_98(int *n)
+void reset_to_98(int *n) /* Description: using pointer as parameter to update value it points to 98
+ * Return: value*/
 {
 *n = 98;
 }
