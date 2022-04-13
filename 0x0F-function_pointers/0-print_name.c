6a91f54379c656527e42d95aa58612eabd7726a5
Benitex44 <benbenedict971@gmail.com>
2022-04-13 19:13:10 +0100

    my last

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
+
 /**
  * print_name - print a name using the function supplied by the caller
  * @name: the name to print
  * @f: the function to call to print the name
  *
  */
+
 void print_name(char *name, void (*f)(char *))
 {
     if (f == NULL)
