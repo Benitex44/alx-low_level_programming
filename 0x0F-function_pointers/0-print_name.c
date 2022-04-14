8897736bed1f02ddaca3218f1f6e3e9dd3ff141b
Benitex44 <benbenedict971@gmail.com>
2022-04-14 06:28:03 +0100

    my print f

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
  * print_name - print a name using the function supplied by the caller
  * @name: the name to print
  * @f: the function to call to print the name
- *
  */
 void print_name(char *name, void (*f)(char *))
 {
